2d9d8f7831715f02b0161b9e7188c523e855367d
Alexander Martinkevich <amartinkevich@bridgeline.com>
2024-02-28 17:03:36 +0300

    feat: remove widget initialization from app.js

--- app.js
+++ app.js
@@ -1,24 +1,4 @@
 import HawksearchVue from '@hawksearch/vue';
-import {ResultItem, ResultListing} from '@hawksearch/vue';
-
-ResultItem.templateOverride = "#vue-hawksearch-result-item";
-ResultListing.templateOverride = "#vue-hawksearch-result-listing";
 
 import '@hawksearch/vue/dist/vue-hawksearch.css';
 import './app.scss';
-
-setTimeout(() => {
-    var components = document.querySelectorAll('[data-vue-hawksearch-component]');
-
-    if (components) {
-        components.forEach(component => {
-            try {
-                var configId = component.getAttribute('data-vue-hawksearch-config');
-                var config = JSON.parse(document.getElementById(configId).innerHTML);
-                var widget = HawksearchVue.createWidget(component, {config, dataLayer: configId});
-            } catch (e) {
-                console.error(e);
-            }
-        });
-    }
-}, 10);
